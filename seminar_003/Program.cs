552cb661a75f4c37f7261a2bf20ab09bd77068a4
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-22 21:00:01 +0500

    num 1 ><num2<>num3(2)

--- seminar_003/Program.cs
+++ seminar_003/Program.cs
@@ -1,19 +1,20 @@
 ﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 Console.Write("Введите первое число: ");
- int numer1= int.Parse(Console.ReadLine());
+ int number1= int.Parse(Console.ReadLine());
 Console.Write ("Введите второе число: ");
-int numer2= int.Parse(Console.ReadLine());
+int number2= int.Parse(Console.ReadLine());
 Console.Write ("Введите третье число: ");
-int numer3= int.Parse(Console.ReadLine());
+int number3= int.Parse(Console.ReadLine());
  int max = number1;
 if (max< number2);
  {
     max=number2;
  }
-if (max< number3) int.MaxValue = num2;
+if (max< number3) ;
 {
     max= number3;
 }
 Console.WriteLine( $" максимальное число = { max } " );
+ 
  
