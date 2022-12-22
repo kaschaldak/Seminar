c57e6aefdc5b8270c6426a133bc02769487667d5
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-22 21:45:42 +0500

    even numbers N

---
+++ Seminar_005/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write ("Введите N: ");
+int N= int.Parse(Console.ReadLine());
+int i = 2;
+while(i <= N)
+{
+    Console.WriteLine($"{i}");
+    i +=2;
+}
